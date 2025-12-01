2ca9b9eb86a73c29c7e5cc14e20b4072fa311113
Andry22A <andridelarosa2020@gmail.com>
2025-12-01 09:59:33 -0400

    C:\Users\DELL\Documents\GitHub\Sistema-Manejo-Empleados

--- SistemaManejoEmpleados/SistemaManejoEmpleados/FrmMain.Designer.cs
+++ SistemaManejoEmpleados/SistemaManejoEmpleados/FrmMain.Designer.cs
@@ -28,12 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.empleadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.departamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mostrarEmpleadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarEmpleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actualizarEmpleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarEmpleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mostrarDepartamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarDepartamentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actualizarDepartamentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarDepartamentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mostrarCargosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarCargoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actualizarCargoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarCargToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.empleadosToolStripMenuItem,
+            this.departamentosToolStripMenuItem,
+            this.cargosToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // empleadosToolStripMenuItem
+            // 
+            this.empleadosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mostrarEmpleadosToolStripMenuItem,
+            this.agregarEmpleadoToolStripMenuItem,
+            this.actualizarEmpleadoToolStripMenuItem,
+            this.eliminarEmpleadoToolStripMenuItem});
+            this.empleadosToolStripMenuItem.Name = "empleadosToolStripMenuItem";
+            this.empleadosToolStripMenuItem.Size = new System.Drawing.Size(116, 30);
+            this.empleadosToolStripMenuItem.Text = "Empleados";
+            this.empleadosToolStripMenuItem.Click += new System.EventHandler(this.empleadosToolStripMenuItem_Click);
+            // 
+            // departamentosToolStripMenuItem
+            // 
+            this.departamentosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mostrarDepartamentosToolStripMenuItem,
+            this.agregarDepartamentoToolStripMenuItem,
+            this.actualizarDepartamentoToolStripMenuItem,
+            this.eliminarDepartamentoToolStripMenuItem});
+            this.departamentosToolStripMenuItem.Name = "departamentosToolStripMenuItem";
+            this.departamentosToolStripMenuItem.Size = new System.Drawing.Size(151, 30);
+            this.departamentosToolStripMenuItem.Text = "Departamentos";
+            // 
+            // cargosToolStripMenuItem
+            // 
+            this.cargosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mostrarCargosToolStripMenuItem,
+            this.agregarCargoToolStripMenuItem,
+            this.actualizarCargoToolStripMenuItem,
+            this.eliminarCargToolStripMenuItem});
+            this.cargosToolStripMenuItem.Name = "cargosToolStripMenuItem";
+            this.cargosToolStripMenuItem.Size = new System.Drawing.Size(84, 29);
+            this.cargosToolStripMenuItem.Text = "Cargos";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem1});
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(61, 29);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // mostrarEmpleadosToolStripMenuItem
+            // 
+            this.mostrarEmpleadosToolStripMenuItem.Name = "mostrarEmpleadosToolStripMenuItem";
+            this.mostrarEmpleadosToolStripMenuItem.Size = new System.Drawing.Size(275, 34);
+            this.mostrarEmpleadosToolStripMenuItem.Text = "Mostrar Empleados";
+            // 
+            // agregarEmpleadoToolStripMenuItem
+            // 
+            this.agregarEmpleadoToolStripMenuItem.Name = "agregarEmpleadoToolStripMenuItem";
+            this.agregarEmpleadoToolStripMenuItem.Size = new System.Drawing.Size(275, 34);
+            this.agregarEmpleadoToolStripMenuItem.Text = "Agregar Empleado";
+            // 
+            // actualizarEmpleadoToolStripMenuItem
+            // 
+            this.actualizarEmpleadoToolStripMenuItem.Name = "actualizarEmpleadoToolStripMenuItem";
+            this.actualizarEmpleadoToolStripMenuItem.Size = new System.Drawing.Size(275, 34);
+            this.actualizarEmpleadoToolStripMenuItem.Text = "Actualizar empleado";
+            // 
+            // eliminarEmpleadoToolStripMenuItem
+            // 
+            this.eliminarEmpleadoToolStripMenuItem.Name = "eliminarEmpleadoToolStripMenuItem";
+            this.eliminarEmpleadoToolStripMenuItem.Size = new System.Drawing.Size(275, 34);
+            this.eliminarEmpleadoToolStripMenuItem.Text = "Eliminar empleado";
+            // 
+            // mostrarDepartamentosToolStripMenuItem
+            // 
+            this.mostrarDepartamentosToolStripMenuItem.Name = "mostrarDepartamentosToolStripMenuItem";
+            this.mostrarDepartamentosToolStripMenuItem.Size = new System.Drawing.Size(308, 34);
+            this.mostrarDepartamentosToolStripMenuItem.Text = "Mostrar Departamentos";
+            // 
+            // agregarDepartamentoToolStripMenuItem
+            // 
+            this.agregarDepartamentoToolStripMenuItem.Name = "agregarDepartamentoToolStripMenuItem";
+            this.agregarDepartamentoToolStripMenuItem.Size = new System.Drawing.Size(308, 34);
+            this.agregarDepartamentoToolStripMenuItem.Text = "Agregar Departamento";
+            // 
+            // actualizarDepartamentoToolStripMenuItem
+            // 
+            this.actualizarDepartamentoToolStripMenuItem.Name = "actualizarDepartamentoToolStripMenuItem";
+            this.actualizarDepartamentoToolStripMenuItem.Size = new System.Drawing.Size(308, 34);
+            this.actualizarDepartamentoToolStripMenuItem.Text = "Actualizar departamento";
+            // 
+            // eliminarDepartamentoToolStripMenuItem
+            // 
+            this.eliminarDepartamentoToolStripMenuItem.Name = "eliminarDepartamentoToolStripMenuItem";
+            this.eliminarDepartamentoToolStripMenuItem.Size = new System.Drawing.Size(308, 34);
+            this.eliminarDepartamentoToolStripMenuItem.Text = "Eliminar departamento";
+            // 
+            // mostrarCargosToolStripMenuItem
+            // 
+            this.mostrarCargosToolStripMenuItem.Name = "mostrarCargosToolStripMenuItem";
+            this.mostrarCargosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.mostrarCargosToolStripMenuItem.Text = "Mostrar Cargos ";
+            // 
+            // agregarCargoToolStripMenuItem
+            // 
+            this.agregarCargoToolStripMenuItem.Name = "agregarCargoToolStripMenuItem";
+            this.agregarCargoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.agregarCargoToolStripMenuItem.Text = "Agregar Cargo";
+            // 
+            // actualizarCargoToolStripMenuItem
+            // 
+            this.actualizarCargoToolStripMenuItem.Name = "actualizarCargoToolStripMenuItem";
+            this.actualizarCargoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.actualizarCargoToolStripMenuItem.Text = "Actualizar Cargo";
+            // 
+            // eliminarCargToolStripMenuItem
+            // 
+            this.eliminarCargToolStripMenuItem.Name = "eliminarCargToolStripMenuItem";
+            this.eliminarCargToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.eliminarCargToolStripMenuItem.Text = "Eliminar Cargo";
+            // 
+            // salirToolStripMenuItem1
+            // 
+            this.salirToolStripMenuItem1.Name = "salirToolStripMenuItem1";
+            this.salirToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.salirToolStripMenuItem1.Text = "Salir";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
             this.Text = "FrmMain";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem empleadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem departamentosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mostrarEmpleadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarEmpleadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actualizarEmpleadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarEmpleadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mostrarDepartamentosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarDepartamentoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actualizarDepartamentoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarDepartamentoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mostrarCargosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarCargoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actualizarCargoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarCargToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem1;
     }
 }
--- SistemaManejoEmpleados/SistemaManejoEmpleados/FrmMain.cs
+++ SistemaManejoEmpleados/SistemaManejoEmpleados/FrmMain.cs
@@ -1,20 +1,135 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SistemaManejoEmpleados;
+using System;
 using System.Windows.Forms;
 
-namespace SistemaManejoEmpleados
+namespace ProyectoEmpleados
 {
     public partial class FrmMain : Form
     {
         public FrmMain()
         {
             InitializeComponent();
+            this.IsMdiContainer = true; // Convierte este formulario en MDI
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+        }
+
+        // -------------------------
+        //  MENU: EMPLEADOS
+        // -------------------------
+
+        private void mostrarEmpleadosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmEmpleadosList frm = new FrmEmpleadosList();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void agregarEmpleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmEmpleadoAgregar frm = new FrmEmpleadoAgregar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void actualizarEmpleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmEmpleadoActualizar frm = new FrmEmpleadoActualizar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void eliminarEmpleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmEmpleadoEliminar frm = new FrmEmpleadoEliminar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        // -------------------------
+        //  MENU: DEPARTAMENTOS
+        // -------------------------
+
+        private void mostrarDepartamentosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmDepartamentosList frm = new FrmDepartamentosList();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void agregarDepartamentoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmDepartamentoAgregar frm = new FrmDepartamentoAgregar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void actualizarDepartamentoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmDepartamentoActualizar frm = new FrmDepartamentoActualizar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void eliminarDepartamentoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmDepartamentoEliminar frm = new FrmDepartamentoEliminar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        // -------------------------
+        //  MENU: CARGOS
+        // -------------------------
+
+        private void mostrarCargosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmCargosList frm = new FrmCargosList();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void agregarCargoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmCargoAgregar frm = new FrmCargoAgregar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void actualizarCargoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmCargoActualizar frm = new FrmCargoActualizar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void eliminarCargoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmCargoEliminar frm = new FrmCargoEliminar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        // -------------------------
+        //  MENU: EXPORTAR
+        // -------------------------
+
+        private void exportarTodoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmExportar frm = new FrmExportar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        // -------------------------
+        //  MENU: SALIR
+        // -------------------------
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
